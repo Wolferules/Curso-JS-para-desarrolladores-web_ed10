fa00af3fd1db8f7d06ba68678cbcf899383eb922
FernandoFreire <fernando.freire@birchmangroup.com>
2018-02-14 21:54:23 +0100

    Añadido ejercicio invitaciones sin acabar

---
+++ FernandoFreire/EjerciciosDia7/ejercicioInvitaciones.js
@@ -0,0 +1,39 @@
+var dimeNombre = prompt("Dime tu nombre");
+var estaIncluidoEvento = prompt("Quiere darse de baja o de alta al evento");
+var listaIds = [];
+
+if (estaIncluidoEvento == "baja") {
+	darDeBaja(dimeNombre);
+} else if (estaIncluidoEvento == "alta") {
+	darDeAlta(dimeNombre);
+} else { console.log("El campo de baja es incorrecto");}
+
+
+	function darDeBaja(nombre) {
+		for(var i = 0; i <= listaIds.length ; i++) {
+			if (listaIds[i] == dimeId) {
+				listaIds[i] == undefined;
+				console.log("Usted, con el nombre " + dimeNombre + " ha sido eliminado.");
+			} else {
+				alert("El nombre que me has dado no aparece entre los invitados");
+				break;
+			}
+		}
+	}
+
+	function darDeAlta(nombre) {
+		for (var i = 0; i <= listaIds.length; i++) {
+				console.log(i);
+				if(listaIds[i] == undefined) {
+					listaIds.push(dimeNombre);
+					console.log("Su nombre ha sido incluido en el evento");
+				}	
+			}	
+	}
+
+
+
+
+
+
+
